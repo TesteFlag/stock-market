62929f10397dab819b869a9a13c9fc7bfee48033
John Doe <john@doe.org>
2022-04-20 19:55:22 +0100

    Implementado formulário preliminar na página de criação de novo stock e eventos sobre o mesmo

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,5 +1,39 @@
 export default function Create() {
+
+    function handleChange(event) {
+        console.log("handleChange: ", event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("submit");
+    }
+
     return (
-        <div>Create</div>
+        <main>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>
+                        Nome
+                        <input type="text" name="name" required minLength="4" maxLength="60" onChange={handleChange} />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Código
+                        <input type="text" name="code" required minLength="3" maxLength="3" onChange={handleChange} />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Preço
+                        <input type="number" name="price" required min="1" max="9999" onChange={handleChange} />
+                    </label>
+                </div>
+                <div>
+                    <button type="submit">Guardar</button>
+                </div>
+            </form>
+        </main>
     )
 }
