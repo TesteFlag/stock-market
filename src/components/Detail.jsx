bf8268a9bf27147537fd7b82082a12f079539ed2
John Doe <john@doe.org>
2022-04-20 21:24:20 +0100

    Melhorado página de detalhe para ter mensagens de não encontrado e a aguardar carregamento

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -20,23 +20,37 @@ export default function Detail() {
     }));
   }
 
+  function Display() {
+    if(stock) {
+
+      if(stock.code) {
+        return (
+          <section>
+            <h2>{ stock.name }</h2>
+            <h3>{ stock.code }</h3>
+            <div>
+                Preço: { stock.price }€
+            </div>
+            <div>
+              <button onClick={toggleFavorite}>
+                {stock.isFavorite ? "Remover " : "Marcar "}
+                Favorito
+              </button>
+            </div>
+          </section>
+        );
+      }
+      else {
+        return (<p>Stock inexistente</p>);
+      }
+
+    }
+    else {
+      return (<p>Aguarde um momento</p>);
+    }
+  }
+
   return (
-    <>
-      {stock && (
-        <section>
-          <h2>{ stock.name }</h2>
-          <h3>{ stock.code }</h3>
-          <div>
-              Preço: { stock.price }€
-          </div>
-          <div>
-            <button onClick={toggleFavorite}>
-              {stock.isFavorite ? "Remover " : "Marcar "}
-              Favorito
-            </button>
-          </div>
-        </section>
-      )}
-    </>
+    <Display />
   )
 }
